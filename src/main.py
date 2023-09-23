0ad92886d706bb51dc15a8ba9753a9333d54bd27
Mehdi Hatami <mehdi.hatami@mail.polimi.it>
2023-09-23 11:54:50 +0200

    adding time_calculator.py to utils

--- src/main.py
+++ src/main.py
@@ -2,22 +2,25 @@ from utils.input_validator import get_valid_input
 from game_logic.hint_generator import generate_hint
 from utils.random_generator import random_generator
 from game_logic.score_calculator import scorer
+from utils.time_calculator import timer
 
-
+@timer
 def main():
     correct_answer = random_generator(1, 100)
     print (correct_answer)
     scorerClass = scorer()
-
+    
     while True:
         user_input = get_valid_input(1, 100)
         
         if user_input == correct_answer:
+            
             print('That''s it, you guessed correctly!')
             print(scorerClass.get_score())
             repeat = input('Do you want to play again? (y/n): ')
             if repeat == 'y':
                 main()
+                
             else:
                 break
 
@@ -29,3 +32,4 @@ def main():
 if __name__ == '__main__': 
     main()
 
+print(main.__name__)
---
+++ src/utils/time_calculator.py
@@ -0,0 +1,14 @@
+import time
+
+def timer(func):
+
+    start_time = time.perf_counter()
+    def wrapper(*args, **kwargs):
+
+
+        output = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        print(f'You were playing for {end_time - start_time} seconds!')
+        return output
+    
+    return wrapper
